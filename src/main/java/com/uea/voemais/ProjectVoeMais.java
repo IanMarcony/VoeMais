55f5584f8d2ccdb52885c4aa093e8d96f9670b5b
Ian Salazar <ian.salazar@itriad.org.br>
2022-09-14 17:25:28 -0400

    chore: change print

--- src/main/java/com/uea/voemais/ProjectVoeMais.java
+++ src/main/java/com/uea/voemais/ProjectVoeMais.java
@@ -3,6 +3,6 @@ package com.uea.voemais;
 public class ProjectVoeMais {
 
     public static void main(String[] args) {
-        System.out.println("Voe Mais");
+        System.out.println("Voe ");
     }
 }
